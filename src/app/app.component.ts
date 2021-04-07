bcb633dc1e1c8771a220709a9f4ed0cc9a042bb8
Lalit Radadiya <lalit.radadiya@gatewaygroup.com>
2021-04-07 10:02:56 +0530

    charJs

---
+++ src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+
+import { DataService } from './data.service';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  public barChartOptions: any = {
+    scaleShowVerticalLines: false,
+    responsive: true
+  };
+  public barChartLabels: string[];
+  public barChartType: string = 'bar';
+  public barChartLegend: boolean = true;
+
+  public barChartData: any[] = [
+    { data: [], label: 'Volume Sales' },
+    { data: [], label: 'Value Sales' }
+  ];
+  constructor(private _emp: DataService) {
+  }
+  ngOnInit() {
+    this._emp.getSales().subscribe(data => {
+      this.barChartLabels = Object.keys(data);
+      this.barChartLabels.forEach(label => {
+        this.barChartData[0].data.push(data[label]['volumeSales']);
+        this.barChartData[1].data.push(data[label]['valueSales']);
+      });
+    });;
+  }
+}
